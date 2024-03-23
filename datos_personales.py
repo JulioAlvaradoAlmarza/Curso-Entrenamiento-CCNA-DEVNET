c7c554ec1a0f8795fa9afd48aeee3a453b0af547
NetAcad DEVASC <changeme@example.com>
2024-03-23 15:26:28 +0000

    Agregado script para solicitar datos personales

---
+++ datos_personales.py
@@ -0,0 +1,12 @@
+# Solicitar los datos al usuario
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apellido: ")
+edad = input("Ingresa tu edad: ")
+sede = input("Ingresa tu sede: ")
+
+# Mostrar los datos ingresados por el usuario
+print("\nDatos ingresados:")
+print("Nombre:", nombre)
+print("Apellido:", apellido)
+print("Edad:", edad)
+print("Sede:", sede)
